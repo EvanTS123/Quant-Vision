b4c5f5649207b3185dc13eff433c3d3cabe00dfc
Evan Taylor-Sheriff <evantaylorsheriff@pm.me>
2025-12-29 17:07:27 +0000

    Configure Streamlit page settings
    Added Streamlit configuration for page title and layout.

--- RiskApp/app.py
+++ RiskApp/app.py
@@ -1,5 +1,7 @@
+import streamlit as st
+
+# come first before writing anything to the page
+st.set_page_config(page_title="Quant Vision", layout="wide")
+
 # redirect to first page automatically
 st.switch_page("pages/1_Portfolio_Builder.py")
-
-from helpers import add_big_sidebar_logo
-add_big_sidebar_logo(width_px=220,)
